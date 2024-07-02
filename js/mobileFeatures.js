e391c8b335a102a6b92cad7cc8e5c6f7e8417caa
German <germants100@gmail.com>
2024-07-02 16:50:53 -0600

    Adding functionality to buttons of mobile version

---
+++ js/mobileFeatures.js
@@ -0,0 +1,13 @@
+const botonAgregarProducto = document.querySelector("#add");
+const seccionAgregarProducto = document.querySelector("#agregar-prod");
+const botonCerrar = document.querySelector("#close");
+
+botonAgregarProducto.addEventListener('click', () => {
+    seccionAgregarProducto.style.display = "block";
+    botonAgregarProducto.style.display = "none";
+})
+
+botonCerrar.addEventListener('click', () => {
+    seccionAgregarProducto.style.display = "none";
+    botonAgregarProducto.style.display = "block";
+})
